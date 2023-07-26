e82f42d53d88fec00d5c8e42992264bc868e5d4f
Ian Tavares <iantavares2405@gmail.com>
2023-07-26 16:46:07 -0300

    add custom bg

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@ export default {
     fontFamily: {
       fira_code: ['Fira Code', 'sans-serif'],
     },
+    backgroundColor: {
+      primary: 'rgb(239 68 68)',
+      hover_primary: 'rgb(248 113 113)',
+      secondary: 'rgb(5 150 105)',
+      hover_secondary: 'rgb(52 211 153)',
+      white: '#fff',
+    },
   },
   plugins: [],
 }
