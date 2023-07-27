282e2ce1b22cf65b21f50ba90e5f325d00e64f4d
Ian Tavares <iantavares2405@gmail.com>
2023-07-27 17:32:41 -0300

    add Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,16 @@
 import { ComponentProps } from 'react'
 
-type ButtonProps = ComponentProps<'button'>
+type ButtonProps = ComponentProps<'button'> & {
+  color?: 'primary' | 'secondary'
+}
 
-export const Button = (props: ButtonProps) => {
+export const Button = ({ color, ...props }: ButtonProps) => {
   return (
     <button
       {...props}
-      className="bg-secondary hover:bg-hover_secondary w-24 px-8 py-3 rounded-md text-white transition-all flex justify-center"
+      className={`bg-white w-24 px-8 py-2 rounded-md text-black transition-all flex justify-center
+       md:bg-${color} md:hover:bg-hover-${color} md:text-white
+       xl:w-32 xl:py-4 xl:px-12 xl:text-2xl`}
     />
   )
 }
