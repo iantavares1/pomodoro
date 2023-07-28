1dad3257f133e831f8b5109ee9144542ec5ced55
Ian Tavares <iantavares2405@gmail.com>
2023-07-28 16:21:53 -0300

    add background extends

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,16 @@
 export default {
   content: ['./index.html', './src/**/*.tsx'],
   theme: {
+    extend: {
+      backgroundColor: {
+        primary: 'rgb(239 68 68)',
+        secondary: 'rgb(5 150 105)',
+        hover_primary: 'rgb(220 38 38)',
+        hover_secondary: 'rgb(4 120 87)',
+      },
+    },
     fontFamily: {
       fira_code: ['Fira Code', 'sans-serif'],
-    },
-    backgroundColor: {
-      primary: 'rgb(239 68 68)',
-      hover_primary: 'rgb(248 113 113)',
-      secondary: 'rgb(5 150 105)',
-      hover_secondary: 'rgb(52 211 153)',
-      white: '#fff',
     },
     screens: {
       xs: '360px',
